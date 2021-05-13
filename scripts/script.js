cbcf1e66300bfd396c25c37336cb1b2ea1be64bc
Jake Joseph <jakejosephcs@gmail.com>
2021-05-13 18:39:18 -0400

    Add preliminary js

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+// ******** HEADER ******** //
+
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+const aboutBtn = document.getElementById('about-btn');
+aboutBtn.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+const experience = document.getElementById('experience-btn');
+experience.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+const work = document.getElementById('work-btn');
+work.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+const contact = document.getElementById('contact-btn');
+contact.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+const resume = document.getElementById('resume-btn');
+resume.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+});
+
+// EXPERIENCE
+const huskyBtn = document.getElementById('husky-btn');
+const niigonBtn = document.getElementById('niigon-btn');
+
+const huskyInfo = document.getElementById('husky-info');
+const niigonInfo = document.getElementById('niigon-info');
+
+huskyBtn.addEventListener('click', () => {
+  huskyBtn.classList.add('active-btn');
+  niigonBtn.classList.remove('active-btn');
+  huskyInfo.classList.remove('hide');
+  niigonInfo.classList.add('hide');
+});
+
+niigonBtn.addEventListener('click', () => {
+  huskyBtn.classList.remove('active-btn');
+  niigonBtn.classList.add('active-btn');
+  huskyInfo.classList.add('hide');
+  niigonInfo.classList.remove('hide');
+});
